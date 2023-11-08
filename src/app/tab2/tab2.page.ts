01701fe700a88f671103bfce7e273f0215e313bd
David_Holliday <dhholliday@icloud.com>
2023-11-08 10:50:49 +0000

    Build and run on physical ios device, iphone. Also add some android (not updated).

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { PhotoService } from '../services/photo.service';
+import { PhotoService, UserPhoto } from '../services/photo.service';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,12 +8,40 @@ import { PhotoService } from '../services/photo.service';
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-  constructor(public photoService: PhotoService) {}
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+  ) {}
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
   async ngOnInit() {
     await this.photoService.loadSaved();
+  }
+  //Action sheet function with delete that calls deletePicture() from PhotoService
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            // this.photoService.deletePicture(photo, position);
+          },
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            // Nothing to do, action sheet is automatically closed
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
   }
 }
